79e87f8eca78be92b8ae54221e0d515b4b375e19
Marco Cavenati s294889 <s294889@studenti.polito.it>
2022-07-25 19:02:06 +0200

    Add weirdshark-cli Add Clap params

---
+++ weirdshark-cli/src/main.rs
@@ -0,0 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Network interface to capture from
+    #[clap(short, long, value_parser)]
+    interface: String,
+
+    /// Output file name
+    #[clap(short, long, value_parser, default_value = "weirdshark_capture")]
+    file_name: String,
+
+    /// Time interval in seconds after which a new report is generated (0 to have only one report at the end)
+    #[clap(short, long, value_parser, default_value_t = 0)]
+    time_interval: usize,
+
+    // TODO Choose filter details in weirdshark lib and implement this param accordingly
+    /// Filter
+    #[clap(short, long, value_parser, default_value = "")]
+    filter: String,
+}
+
+fn main() {
+    let args = Args::parse();
+}
