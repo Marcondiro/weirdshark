574393e8b2cdb3cc53f91fd140bc5dcc57196e64
Dri99 <alessandro.mandrile99@gmail.com>
2022-08-06 13:00:23 +0100

    +created config module

--- weirdshark/src/capturer/mod.rs
+++ weirdshark/src/capturer/mod.rs
@@ -11,105 +11,8 @@ use pnet::datalink::{channel, interfaces, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
 use pnet::packet::ethernet;
 use crate::{get_interface_by_description, get_interface_by_name, handle_ethernet_frame, write_csv};
+use crate::capturer::config::CaptureConfig;
 
-
-pub struct CaptureConfig {
-    interface: Option<NetworkInterface>,
-    report_path: Option<PathBuf>,
-    report_interval: Option<time::Duration>,
-    ip_filters: LinkedList<(IpFilter,DirectionFilter)>,
-    //l3_filters : LinkedList<Filter<Ipv4Packet>>,
-    //TODO filters
-}
-
-impl Default for CaptureConfig {
-    fn default() -> Self {
-        let interface = Some(interfaces().into_iter()
-            .filter(|i| !i.is_loopback()
-                && i.is_up()
-                && is_interface_running(i)
-                && !i.ips.is_empty())
-            .next()
-            .expect("Weirdshark cannot find a default interface."));
-        Self {
-            interface,
-            report_path: Some(PathBuf::from("weirdshark_capture.csv")),
-            report_interval: None,
-            ip_filters: LinkedList::new(),
-            //l3_filters : LinkedList::new(),
-        }
-    }
-}
-
-#[cfg(unix)]
-fn is_interface_running(i: &NetworkInterface) -> bool {
-    i.is_running()
-}
-
-#[cfg(not(unix))]
-fn is_interface_running(_i: &NetworkInterface) -> bool {
-    return true;
-}
-
-impl CaptureConfig {
-    pub fn new() -> Self {
-
-        Self {
-            interface: None,
-            report_path: None,
-            report_interval: None,
-            ip_filters: LinkedList::new(),
-            //l3_filters : LinkedList::new(),
-        }
-    }
-
-    //TODO: add fn set interface by description for windows
-
-    pub fn set_interface_by_name(mut self, name: &str) -> Self {
-        self.interface = get_interface_by_name(name);
-            //.expect("Network interface not found"); // TODO: manage this with errors?
-        self
-    }
-
-    pub fn set_interface_by_description(mut self, name: &str) -> Self {
-        self.interface = get_interface_by_description(name);
-            //.expect("Network interface not found"); // TODO: manage this with errors?
-        self
-    }
-
-    pub fn set_interface_by_index(mut self, index: u32) -> Self {
-        let interface = interfaces().into_iter()
-            .filter(|i| i.index == index)
-            .next();
-            //.expect("Network interface not found"); // TODO: manage this with errors
-        self.interface = interface;
-        self
-    }
-
-    pub fn set_report_path(mut self, path: PathBuf ) ->Self{
-        self.report_path = Some(path);
-        self
-    }
-
-    pub fn set_report_interval(mut self, duration: Option<time::Duration>) -> Self {
-        self.report_interval = duration;
-        self
-    }
-
-    pub fn add_filter_ip_addr(mut self, addr: IpAddr, ) -> Self {
-        let filter = IpFilter::Single(addr);
-        self.ip_filters.push_back((filter,DirectionFilter::Both) );
-        self
-    }
-
-    pub fn build_capturer(self) -> Result<CaptureController, WeirdsharkError> {
-        //TODO: this should check thath all Configs are correct
-        let (sender, receiver) = std::sync::mpsc::channel();
-        let t_sender = sender.clone();
-        let thread_handle = std::thread::spawn(move || capture_thread_fn(self, t_sender, receiver));
-        Ok(CaptureController { sender, thread_handle })
-    }
-}
 
 pub enum WorkerCommand {
     Start,
@@ -209,7 +112,7 @@ fn pnet_capture_adapter(interface: &NetworkInterface, sender: &Sender<WorkerComm
     });
 }
 
-enum IpFilter {
+pub enum IpFilter {
     Single(IpAddr),
     Range(IpAddr, IpAddr),
     List(Vec<IpAddr>),
@@ -226,7 +129,7 @@ impl IpFilter {
     }
 }
 
-enum DirectionFilter {
+pub enum DirectionFilter {
     Source,
     Destination,
     Both,
@@ -235,5 +138,114 @@ enum DirectionFilter {
 #[derive(Debug)]
 pub enum WeirdsharkError {
     GenericError,
+}
+
+mod config{
+    use std::collections::LinkedList;
+    use std::net::IpAddr;
+    use std::path::PathBuf;
+    use std::time;
+    use pnet::datalink;
+    use pnet::datalink::NetworkInterface;
+    use crate::capturer::{capture_thread_fn, CaptureController, DirectionFilter, IpFilter, WeirdsharkError};
+    use crate::{get_interface_by_description, get_interface_by_name};
+
+    pub struct CaptureConfig {
+        pub(crate) interface: Option<NetworkInterface>,
+        pub(crate) report_path: Option<PathBuf>,
+        pub(crate) report_interval: Option<time::Duration>,
+        pub(crate) ip_filters: LinkedList<(IpFilter,DirectionFilter)>,
+        //l3_filters : LinkedList<Filter<Ipv4Packet>>,
+        //TODO filters
+    }
+
+    impl Default for CaptureConfig {
+        fn default() -> Self {
+            let interface = Some(datalink::interfaces().into_iter()
+                .filter(|i| !i.is_loopback()
+                    && i.is_up()
+                    && is_interface_running(i)
+                    && !i.ips.is_empty())
+                .next()
+                .expect("Weirdshark cannot find a default interface."));
+            Self {
+                interface,
+                report_path: Some(PathBuf::from("weirdshark_capture.csv")),
+                report_interval: None,
+                ip_filters: LinkedList::new(),
+                //l3_filters : LinkedList::new(),
+            }
+        }
+    }
+
+    #[cfg(unix)]
+    fn is_interface_running(i: &NetworkInterface) -> bool {
+        i.is_running()
+    }
+
+    #[cfg(not(unix))]
+    fn is_interface_running(_i: &NetworkInterface) -> bool {
+        return true;
+    }
+
+    impl CaptureConfig {
+        pub fn new() -> Self {
+
+            Self {
+                interface: None,
+                report_path: None,
+                report_interval: None,
+                ip_filters: LinkedList::new(),
+                //l3_filters : LinkedList::new(),
+            }
+        }
+
+        //TODO: add fn set interface by description for windows
+
+        pub fn set_interface_by_name(mut self, name: &str) -> Self {
+            self.interface = get_interface_by_name(name);
+            //.expect("Network interface not found"); // TODO: manage this with errors?
+            self
+        }
+
+        pub fn set_interface_by_description(mut self, name: &str) -> Self {
+            self.interface = get_interface_by_description(name);
+            //.expect("Network interface not found"); // TODO: manage this with errors?
+            self
+        }
+
+        pub fn set_interface_by_index(mut self, index: u32) -> Self {
+            let interface = datalink::interfaces().into_iter()
+                .filter(|i| i.index == index)
+                .next();
+            //.expect("Network interface not found"); // TODO: manage this with errors
+            self.interface = interface;
+            self
+        }
+
+        pub fn set_report_path(mut self, path: PathBuf ) ->Self{
+            self.report_path = Some(path);
+            self
+        }
+
+        pub fn set_report_interval(mut self, duration: Option<time::Duration>) -> Self {
+            self.report_interval = duration;
+            self
+        }
+
+        pub fn add_filter_ip_addr(mut self, addr: IpAddr, ) -> Self {
+            let filter = IpFilter::Single(addr);
+            self.ip_filters.push_back((filter,DirectionFilter::Both) );
+            self
+        }
+
+        pub fn build_capturer(self) -> Result<CaptureController, WeirdsharkError> {
+            //TODO: this should check that all Configs are correct
+            let (sender, receiver) = std::sync::mpsc::channel();
+            let t_sender = sender.clone();
+            let thread_handle = std::thread::spawn(move || capture_thread_fn(self, t_sender, receiver));
+            Ok(CaptureController { sender, thread_handle })
+        }
+    }
 }
 
