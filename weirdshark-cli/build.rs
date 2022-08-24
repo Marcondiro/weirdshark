44404540ea9f9cb803b8ef5a035e0082fa8522fe
Marco Cavenati s294889 <s294889@studenti.polito.it>
2022-08-24 12:15:59 +0200

    Add automatic manual generation

--- weirdshark-cli/build.rs
+++ weirdshark-cli/build.rs
@@ -1,7 +1,22 @@
+include!("./src/args/mod.rs");
+use clap::CommandFactory;
+
 fn main() {
     if cfg!(windows) {
         println!("cargo:rustc-link-search=all=../weirdshark/lib");
         println!("cargo:rustc-link-search=all=../weirdshark/lib/x64");
         println!("cargo:rustc-link-lib=static=packet");
     }
+
+    // Automatically generate manual
+    let out_dir = std::path::PathBuf::from(
+        std::env::var_os("OUT_DIR").expect(std::io::ErrorKind::NotFound.to_string().as_str())
+    );
+
+    let cmd = Cli::command();
+    let man = clap_mangen::Man::new(cmd);
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer).unwrap();
+
+    std::fs::write(out_dir.join("weirdshark-cli.1"), buffer).unwrap();
 }
